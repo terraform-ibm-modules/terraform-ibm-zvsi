60859088699d5d861e4ff927b62daab3b044c10c
root <ketaki.patil1@ibm.com>
2023-12-08 12:55:51 +0000

    client-to-site vpn setup added

---
+++ solutions/standard/provider.tf
@@ -0,0 +1,5 @@
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.existing_sm_instance_region == null ? var.region : var.existing_sm_instance_region
+  alias            = "ibm-sm"
+}
--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -30,8 +30,94 @@ variable "ssh_public_key" {
   }
 }
 
+variable "existing_sm_instance_guid" {
+  type        = string
+  description = "Existing Secrets Manager GUID. The existing Secret Manager instance must have private certificate engine configured. If not provided an new instance will be provisioned."
+  default     = null
+}
+
+variable "existing_sm_instance_region" {
+  type        = string
+  description = "Required if value is passed into var.existing_sm_instance_guid"
+  default     = null
+}
+
 variable "override" {
 description = "Override default values with custom JSON template. This uses the file `override.json` to allow users to create a fully customized environment."
 type        = bool
 default     = true
+}
+
+variable "sm_service_plan" {
+  type        = string
+  description = "Type of service plan to use to provision Secrets Manager if not using an existing one."
+  default     = "standard"
+}
+
+variable "root_ca_name" {
+  type        = string
+  description = "Name of the Root CA to create for a private_cert secret engine. Only used when var.existing_sm_instance_guid is false"
+  default     = "root-ca"
+}
+
+variable "intermediate_ca_name" {
+  type        = string
+  description = "Name of the Intermediate CA to create for a private_cert secret engine. Only used when var.existing_sm_instance_guid is false"
+  default     = "intermediate-ca"
+}
+
+variable "certificate_template_name" {
+  type        = string
+  description = "Name of the Certificate Template to create for a private_cert secret engine. When var.existing_sm_instance_guid is true, then it has to be the existing template name that exists in the private cert engine."
+  default     = "my-template"
+}
+
+variable "create_policy" {
+  description = "Set to true to create a new access group (using the value of var.access_group_name) with a VPN Client role"
+  type        = bool
+  default     = true
+}
+
+variable "vpn_client_access_group_users" {
+  description = "List of users in the Client to Site VPN Access Group"
+  type        = list(string)
+  default     = []
+}
+
+variable "access_group_name" {
+  type        = string
+  description = "Name of the IAM Access Group to create if var.create_policy is true"
+  default     = "client-to-site-vpn-access-group"
+}
+
+variable "vpn_server_routes" {
+  type = map(object({
+    destination = string
+    action      = string
+  }))
+  description = "Map of server routes to be added to created VPN server."
+  default = {
+    "vpc-10" = {
+      destination = "10.0.0.0/8"
+      action      = "deliver"
+    }
+  }
+}
+
+variable "root_ca_max_ttl" {
+  type        = string
+  description = "Maximum TTL value for the root CA"
+  default     = "8760h"
+}
+
+variable "root_ca_common_name" {
+  type        = string
+  description = "Fully qualified domain name or host domain name for the certificate to be created"
+  default     = "cloud.ibm.com"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "Name of the resource group to use for this example. If not set, a resource group is created."
+  default = null
 }
