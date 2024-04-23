575cf001906abb2efe5b6927162422311eee530b
Pketaki <144440077+Pketaki@users.noreply.github.com>
2024-04-23 18:42:36 +0530

    Update variables.tf chnaged default value of machine_type to "mz2-2x16"

--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -37,7 +37,7 @@ variable "ssh_public_key" {
 
 variable "machine_type" {
   type = string 
-  default = "bz2-4x16"
+  default = "mz2-2x16"
   description = "input machine type: valid values are: bz2-4x16, bz2-8x32, bz2-16x64, cz2-8x16, cz2-16x32, mz2-2x16, mz2-4x32, mz2-8x64, mz2-16x128"
   validation {
     condition  = contains(["mz2o-2x16","bz2-4x16", "bz2-8x32", "bz2-16x64", "cz2-8x16", "cz2-16x32", "mz2-2x16", "mz2-4x32", "mz2-8x64", "mz2-16x128"], var.machine_type)
