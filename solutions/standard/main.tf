68c346be75bed7a4a8fde0c800c065447150df68
Pradeep Kadiyala <pkadiya@us.ibm.com>
2024-07-25 14:06:23 -0500

    fix: forgot to refer to data while assigning the subnet with subnet id

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -15,9 +15,9 @@ locals {
 module "landing_zone" {
   source               = "terraform-ibm-modules/landing-zone/ibm//patterns//vsi//module"
   version              = "5.22.0"
-  prefix               = var.prefix
-  region               = var.region
-  ssh_public_key       = var.ssh_public_key  
+  prefix               = "pkwaazi"
+  region               = "us-east"
+  ssh_public_key       = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDWMalt0l428a0yK/Tce3H5lwFBnw6U7lshLCHB4s+SuOXGHYvQxKWZXAOKZocp/nUjnflfbOF1Wa15NOBnNHI00HmmsoRGuG36o2nqjqyDonGeJZ5HJ1zDaqZ4ZGTSRdUd8zBGazN1pamL5coxmOE22bLnlPelqSz8zhA2mrJXJ85nuHl3tQIhcB6qBP+jpDMxQk7On2E3v4pUhpTH+M+fR4JFVlvMDj2oeHUzrVIkX9VlxClrZSMp6DClp+rGpHSMIGnUc6WFJSRuJx2/iQ4FSaPndE1cRNsg4u4M3K3yj4MwiP1auaBGbXtrsAubLOdm/OnQnapyDUc71f0YN6GkzbgidYHywA65xkkelHGfuCl68eg6S32tQeoRTBKTtu2J0QLv5mZ4WSw7r3/5HvGMBudYTU2ts4nwVHKI6pJXYDoQBRVBioss6W6+jmJrY/6UWTuetutJUzUij05/XiiGUhDsiGJAeaTDH0WBe4qBubpFp1sw0RaGoNq1GNM1UOk= pradeepkadiyala@pradeeps-mbp.lan" 
   override_json_string = local.image
 }
 
@@ -234,7 +234,7 @@ resource "ibm_is_virtual_endpoint_gateway" "vpe_res_ctrl" {
   }
   vpc = data.ibm_is_subnet.workload_vpe_zone2.vpc
   ips {
-    subnet = ibm_is_subnet.workload_vpe_zone2.id
+    subnet = data.ibm_is_subnet.workload_vpe_zone2.id
     name   = "${var.prefix}-vpe-resource-ctrl-ip"
   }
   resource_group = data.ibm_is_subnet.workload_vpe_zone2.resource_group
@@ -248,7 +248,7 @@ resource "ibm_is_virtual_endpoint_gateway" "vpe_iaas" {
   }
   vpc = data.ibm_is_subnet.workload_vpe_zone2.vpc
   ips {
-    subnet = ibm_is_subnet.workload_vpe_zone2.id
+    subnet = data.ibm_is_subnet.workload_vpe_zone2.id
     name   = "${var.prefix}-vpe-iaas-ip"
   }
   resource_group = data.ibm_is_subnet.workload_vpe_zone2.resource_group
