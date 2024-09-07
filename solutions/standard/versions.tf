c5433627ba92708b3e59f37e99e40957540a0e22
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-09-07 11:46:23 +0000

    fix(deps): update required_provider to latest for the deployable architecture solution

--- solutions/standard/versions.tf
+++ solutions/standard/versions.tf
@@ -8,11 +8,11 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.66.0"
+      version = "1.69.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.11.2"
+      version = "0.12.0"
     }
   }
 }
